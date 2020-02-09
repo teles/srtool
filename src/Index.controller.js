710db4fff025b4881518d098b0d2c20cac1d504f
Teles <jose.maciel@vivadecora.com.br>
2020-02-09 19:58:55 -0300

    Commit inicial. Boilerplate de angular com ES6 funcionando

---
+++ src/Index.controller.js
@@ -0,0 +1,7 @@
+const IndexController = function IndexController() {
+    const vc = this;
+    vc.message = "Olá Mundo";
+};
+
+IndexController.$inject = [];
+export default IndexController;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import angular from "angular";
+import IndexController from "./Index.controller";
+
+angular.module("srtool", []);
+angular.module("srtool").controller("IndexController", IndexController);
